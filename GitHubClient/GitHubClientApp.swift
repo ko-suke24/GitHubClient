5db38f2adf778f7bd3d93f97008a4a4927f77554
ko-suke24 <kosuke0322@gmail.com>
2022-06-14 09:51:44 +0900

    Initial Commit

---
+++ GitHubClient/GitHubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubClientApp.swift
+//  GitHubClient
+//
+//  Created by 佐藤航輔 on 2022/06/14.
+//
+
+import SwiftUI
+
+@main
+struct GitHubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
